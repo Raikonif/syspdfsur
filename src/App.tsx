083c84803d329027ff41bacc22d707487925ae9b
Raikonif <raikonif@gmail.com>
2023-04-05 10:55:35 -0400

    first commit project installations

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex">
+      <div className="bg-black h-screen w-full">
+        <h1 className="text-3xl text-slate-500 font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
