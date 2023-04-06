dd406d8be23786a0c79c4868d4a87b5001dd19da
Raikonif <raikonif@gmail.com>
2023-04-06 18:58:07 -0400

    menu options was createed and implemented

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import GeneralMenu from "~/components/GeneralMenu";
 import homeOptions from "~/constants/options/home.options";
-interface IHomeProps {
-  id: number;
-  title: string;
-  icon: string;
-  link: string;
-}
 
 function Home(): JSX.Element {
   return (
