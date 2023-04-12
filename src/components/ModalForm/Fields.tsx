532fa6b152974e29d2cde1d7178916f17c0ff60c
Raikonif <raikonif@gmail.com>
2023-04-12 08:51:15 -0400

    field autocomplete off

--- src/components/ModalForm/Fields.tsx
+++ src/components/ModalForm/Fields.tsx
@@ -31,6 +31,7 @@ function Fields({ itemFields, onInputChange }: IProps): JSX.Element {
           className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
           name={item.name}
           type={item.type}
+          autoComplete="off"
           // placeholder={item.placeholder}
           value={inputValues[index]}
           onChange={(e) => handleInputChange(index, item.name, e.target.value)}
