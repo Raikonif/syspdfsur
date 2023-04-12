3f8f76177fb998f95f0c5731a3763d0666aab3c5
Raikonif <raikonif@gmail.com>
2023-04-12 05:09:50 -0400

    form working well

--- src/components/ModalForm/Fields.tsx
+++ src/components/ModalForm/Fields.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import IFieldProps from "~/interfaces/fieldInterface";
 
@@ -6,18 +6,46 @@ interface IProps {
   itemFields: IFieldProps[];
 }
 function Fields({ itemFields }: IProps): JSX.Element {
-  const listFields: JSX.Element[] = itemFields.map((item: IFieldProps) => (
-    <label className="relative cursor-pointer" id={item.name} key={item.id}>
-      <input
-        name={item.name}
-        type={item.type}
-        placeholder={item.placeholder}
-        className="m-2 h-[60px] w-96 px-6 text-2xl text-violet-500 border-violet-500 border-2 rounded-lg border-opacity-50 outline-none focus:border-violet-600 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
-      />
-      <span className="text-2xl text-violet-500 text-opacity-80 absolute left-5 top-5 px-1 transition duration-200 input-text">
-        {item.placeholder}
-      </span>
-    </label>
+  const [inputValue, setInputValue] = useState<string[]>(new Array(itemFields.length));
+  const handleInputChange = (index: number, value: string) => {
+    const newInputValues: string[] = [...inputValue];
+    newInputValues[index] = value;
+    setInputValue(newInputValues);
+  };
+
+  const listFields: JSX.Element[] = itemFields.map((item: IFieldProps, index: number) => (
+    // <label className="relative cursor-pointer" id={item.name} key={item.id}>
+    //   <input
+    //     name={item.name}
+    //     type={item.type}
+    //     placeholder={item.placeholder}
+    //     className=" m-2 h-[60px] w-full px-6 text-2xl text-violet-500 border-violet-500 border-2 rounded-lg border-opacity-50 outline-none focus:border-violet-600 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
+    //   />
+    //   <span className="text-2xl text-violet-500 text-opacity-80 absolute left-5 top-5 px-1 transition duration-200 input-text">
+    //     {item.placeholder}
+    //   </span>
+    // </label>
+    <div className="md:flex md:items-center mb-6 w-80" key={item.id}>
+      <div className="md:w-1/3">
+        <label
+          className="block text-violet-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+          id={item.name}
+        >
+          {item.placeholder}
+        </label>
+      </div>
+      <div className="md:w-2/3">
+        <input
+          id={item.name}
+          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+          name={item.name}
+          type={item.type}
+          // placeholder={item.placeholder}
+          value={inputValue[index]}
+          onChange={(e) => handleInputChange(index, e.target.value)}
+        />
+      </div>
+    </div>
   ));
   return <>{listFields}</>;
 }
--- src/components/ModalForm/Modal.tsx
+++ src/components/ModalForm/Modal.tsx
@@ -5,12 +5,9 @@ import GeneralForm from "~/components/ModalForm/GeneralForm";
 
 function GeneralModal(): JSX.Element {
   return (
-    <div className="fixed z-10 inset-0 overflow-y-auto">
-      <div className="flex justify-center bg-black items-center inset-0">
-        <div className="fixed inset-0 bg-slate-300 justify-center items-center h-5/6 w-3/4 rounded-lg">
-          <GeneralForm />
-          {/*<Button btnType={loginOption.btnType} textButton={loginOption.buttonName} />*/}
-        </div>
+    <div className="flex bg-slate-500 justify-center items-center">
+      <div className="bg-white rounded-xl w-4/6 opacity-100">
+        <GeneralForm />
       </div>
     </div>
   );
