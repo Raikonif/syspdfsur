5158d60325d77080a20b30739ca56919fba2158c
Raikonif <raikonif@gmail.com>
2023-04-17 21:04:35 -0400

    patients card modified with delete and edit added

--- src/components/PatientCard.tsx
+++ src/components/PatientCard.tsx
@@ -1,33 +1,32 @@
 import React, { useEffect, useState } from "react";
+import { MdEditSquare, RiDeleteBinFill } from "react-icons/all";
 
-function PatientCard(): JSX.Element {
-  const [textColorTitle, setTextColorTitle] = useState<string>("text-fuchsia-600");
-  const [textColorDescription, setTextColorDescription] = useState<string>("text-gray-500");
-  const [textColorDate, setTextColorDate] = useState<string>("text-gray-300");
-  const [isClicked, setIsClicked] = useState<boolean>(false);
-  // useEffect(() => {
-  //   if (isClicked){}
-  // }, [textColorTitle, textColorDescription, textColorDate]);
-  const handleTextColor = () => {
-    setTextColorTitle("text-gray-600");
-    setTimeout(() => {
-      setTextColorTitle("text-fuchsia-600");
-    }, 1500);
-    setTextColorDescription(
-      textColorDescription === "text-gray-500" ? "text-gray-400" : "text-gray-500",
-    );
-    setTextColorDate(textColorDate === "text-gray-300" ? "text-gray-200" : "text-gray-300");
-    // set color default again
-    setTextColorTitle("text-fuchsia-600");
-    setTextColorDescription("text-gray-500");
-    setTextColorDate("text-gray-300");
-  };
+interface IProps {
+  description: string;
+}
+function PatientCard({ description }: IProps): JSX.Element {
+  const [currentDescription, setCurrentDescription] = useState<string>(description);
+  const maxLength = 40;
+  useEffect(() => {
+    if (maxLength < currentDescription.length) {
+      setCurrentDescription(currentDescription.slice(0, maxLength) + "...");
+    }
+  }, [currentDescription]);
+
   return (
-    <div className="duration-300 hover:scale-110" onClick={handleTextColor}>
+    <div className="duration-300 hover:scale-110">
       <article className="container rounded-2xl bg-white p-5 shadow-2xl">
-        <h1 className={`font-bold ${textColorTitle}`}>Patient Card</h1>
-        <p className="font-light text-gray-500 active:text-gray-400">This is a Description</p>
+        <h1 className="font-bold text-fuchsia-600">Patient Card</h1>
+        <p className="font-light text-gray-500 active:text-gray-400">{currentDescription}</p>
         <h6 className="mb-5 text-sm text-gray-300 active:text-gray-200">Date 12/04/2023</h6>
+        <div className="flex justify-between">
+          <button className="h-8 w-7 hover:scale-110">
+            <MdEditSquare className="h-full w-full text-fuchsia-600 hover:text-fuchsia-500" />
+          </button>
+          <button className="h-8 w-7 hover:scale-110">
+            <RiDeleteBinFill className="h-full w-full text-fuchsia-600 hover:text-fuchsia-500" />
+          </button>
+        </div>
       </article>
     </div>
   );
--- src/pages/Patients.tsx
+++ src/pages/Patients.tsx
@@ -2,21 +2,23 @@ import React from "react";
 import PatientCard from "~/components/PatientCard";
 
 function Patients(): JSX.Element {
+  const description =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nisl vi elit. Lorem Ipsum dolor sit amet";
   return (
     <div className="flex items-center justify-center">
       <div className="m-10 mb-10 mt-14 grid h-auto grid-cols-5 justify-between gap-6 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md2:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
+        <PatientCard description={description} />
+        <PatientCard description={description} />
+        <PatientCard description={description} />
+        <PatientCard description={description} />
+        <PatientCard description={description} />
+        <PatientCard description={description} />
+        <PatientCard description={description} />
+        <PatientCard description={description} />
+        <PatientCard description={description} />
+        <PatientCard description={description} />
+        <PatientCard description={description} />
+        <PatientCard description={description} />
       </div>
     </div>
   );
