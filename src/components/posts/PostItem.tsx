6975754401a506a6d84087fc30cf349b12986135
Raikonif <raikonif@gmail.com>
2023-05-20 12:15:26 -0400

    changed style card PostItem

--- src/components/posts/PostItem.tsx
+++ src/components/posts/PostItem.tsx
@@ -9,11 +9,14 @@ interface IProps {
 }
 function PostItem({ title, description, image, date, key }: IProps) {
   return (
-    <li className="m-1 flex flex-col rounded-lg border bg-slate-50 p-1 shadow-xl" key={key}>
+    <li
+      className="m-1 flex w-full flex-col rounded-lg border bg-slate-50 p-1 shadow-xl hover:bg-violet-50 active:bg-violet-200"
+      key={key}
+    >
       <button className="flex flex-col">
-        <span>{title}</span>
-        <span>{description}</span>
-        <span>{date}</span>
+        <span className="text-violet-500">{title}</span>
+        <span className="text-slate-600">{description}</span>
+        <span className="text-slate-500">{date}</span>
       </button>
     </li>
   );
