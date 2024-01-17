5dee66f9b45122e7f9f7928ccb8efdf803d6da7f
Raikonif <raikonif@gmail.com>
2024-01-17 18:39:17 -0400

    menu mobile desing working

--- src/components/blog_menu/HomeMenu.tsx
+++ src/components/blog_menu/HomeMenu.tsx
@@ -18,16 +18,16 @@ function HomeMenu(): ReactElement {
       <div className="bg-violet-600 px-2 py-2 sm:justify-between md:flex md:px-10">
         <button
           onClick={handleGoHome}
-          className="flex cursor-pointer items-center text-2xl font-bold text-gray-800"
+          className="mb-3 flex cursor-pointer items-center text-2xl font-bold text-gray-800"
         >
           <GiMicroscope className="mx-2 text-white" size={40} />
           <h3 className="text-white">PANDY BLOG</h3>
         </button>
-        <ul className="flex">
+        <ul className="my-2 flex w-auto items-center justify-between text-xs">
           {homeOptions.blogOptions.map((item: IMenuInterface) => (
             <NavLink
               className={({ isActive }): string =>
-                `mx-5 rounded-full text-lg duration-500 ${
+                `mx-1 cursor-pointer rounded-full text-lg duration-500 sm:mx-5 ${
                   isActive
                     ? "bg-slate-200 text-violet-600"
                     : "text-lg text-white hover:bg-slate-100 hover:text-violet-400"
@@ -36,7 +36,7 @@ function HomeMenu(): ReactElement {
               key={item.id}
               to={item.link}
             >
-              <li className="m-2 px-5 text-lg font-semibold focus:text-violet-600">
+              <li className="m-1 px-5 font-semibold focus:text-violet-600  sm:m-2 sm:text-lg">
                 {t(item.title.toUpperCase())}
               </li>
             </NavLink>
