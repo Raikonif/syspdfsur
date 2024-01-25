0ef45f19e914f187f214695af93ba401ac60db86
Raikonif <raikonif@gmail.com>
2024-01-25 16:30:23 -0400

    implemented image default by lazyloading

--- src/pages/blog_client/Home/components/articles/ArticleCard.tsx
+++ src/pages/blog_client/Home/components/articles/ArticleCard.tsx
@@ -1,13 +1,31 @@
-import React from "react";
-import { Article } from "~/interfaces/Article.interface";
-
+import React, { useEffect, useState } from "react";
+import { Article, Image } from "~/interfaces/Article.interface";
+import default_image from "~/assets/default_image.jpg";
 function ArticleCard({ id, title, author, date, content, images, type }: Article) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [imageSrcLoaded, setImageSrcLoaded] = useState<string>("");
   const slicedContent = content.slice(0, 80) + " ...";
-  const image = images[0];
+  const image: Image = images[0];
   const openArticle = () => {
     const articleUrl = `/article/${encodeURIComponent(id)}`;
     window.open(articleUrl, "_blank");
   };
+
+  useEffect(() => {
+    const loadImage = async () => {
+      try {
+        const response = await fetch(image.url);
+        const blob = await response.blob();
+        const objectURL = URL.createObjectURL(blob);
+        setImageSrcLoaded(objectURL);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    loadImage();
+  }, [image]);
+
   return (
     <article
       className="my-4 flex w-full cursor-pointer flex-col rounded-md border-2 bg-white p-4 pt-7 shadow-2xl dark:border-slate-600 dark:bg-slate-700 dark:text-white"
@@ -17,11 +35,20 @@ function ArticleCard({ id, title, author, date, content, images, type }: Article
       <p className="font-semibold text-slate-400">{date}</p>
       <h3 className="font-semibold text-violet-600 dark:text-fuchsia-500">{author}</h3>
       <p className="mb-2 mt-1">{slicedContent}</p>
-      <img
-        src={image.url}
-        alt={String(image.id)}
-        className="max-h-[200px] w-full rounded-md border-2"
-      />
+
+      {!isLoading ? (
+        <img
+          src={imageSrcLoaded}
+          alt={String(image.id)}
+          className="max-h-[200px] w-full rounded-md border-2"
+        />
+      ) : (
+        <img
+          src={default_image}
+          alt={"default_image"}
+          className="max-h-[200px] w-full rounded-md border-2"
+        />
+      )}
     </article>
   );
 }
