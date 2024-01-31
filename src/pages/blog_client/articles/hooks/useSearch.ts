0be4d603fb1823081d6dfbaa3631ee834c2956b0
Raikonif <raikonif@gmail.com>
2024-01-30 20:16:38 -0400

    _redirects.html for netlify added

--- src/pages/blog_client/articles/hooks/useSearch.ts
+++ src/pages/blog_client/articles/hooks/useSearch.ts
@@ -1,45 +1,32 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { ALL, CITHOLOGY, HISTOPATHOLOGY, PAP } from "~/constants/Blog/blog.constants";
 import { Article } from "~/interfaces/Article.interface";
-import articles from "~/pages/blog_client/articles/Articles";
 
 function useSearch({
   articles,
   changeSearch,
-  changeFilter,
 }: {
   articles: Article[];
   changeSearch: string;
   changeFilter: string;
 }): Article[] {
   const [filteredArticles, setFilteredArticles] = useState<Article[]>([] as Article[]);
-  useEffect(() => {
-    if (Array.isArray(articles) && articles.length > 0) {
-      changeSearch !== "" &&
-      changeSearch !== undefined &&
-      changeSearch !== null &&
-      changeSearch !== " " &&
-      changeSearch !== ALL &&
-      changeSearch !== PAP &&
-      changeSearch !== CITHOLOGY &&
-      changeSearch !== HISTOPATHOLOGY
-        ? setFilteredArticles(
-            articles.filter((article) =>
-              article.title.toUpperCase().includes(changeSearch.toUpperCase()),
-            ),
-          )
-        : setFilteredArticles(articles);
-    }
-  }, [articles, changeSearch]);
-  useEffect(() => {
-    if (Array.isArray(articles) && articles.length > 0) {
-      changeFilter !== ALL
-        ? setFilteredArticles(
-            articles.filter((article) => article.type.toUpperCase() === changeFilter),
-          )
-        : setFilteredArticles(articles);
-    }
-  }, [articles, changeFilter]);
+  if (Array.isArray(articles) && articles.length > 0) {
+    changeSearch !== "" &&
+    changeSearch !== undefined &&
+    changeSearch !== null &&
+    changeSearch !== " " &&
+    changeSearch !== ALL &&
+    changeSearch !== PAP &&
+    changeSearch !== CITHOLOGY &&
+    changeSearch !== HISTOPATHOLOGY
+      ? setFilteredArticles(
+          articles.filter((article) =>
+            article.title.toUpperCase().includes(changeSearch.toUpperCase()),
+          ),
+        )
+      : setFilteredArticles(articles);
+  }
   return filteredArticles;
 }
 
