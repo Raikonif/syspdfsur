2076a683ceb31a235f5526ec71f7aed20aef68ab
Raikonif <raikonif@gmail.com>
2024-02-15 20:52:22 -0400

    added quill library for rich editor text to create the articles

--- src/pages/articles/ModalCreateArticle.tsx
+++ src/pages/articles/ModalCreateArticle.tsx
@@ -14,15 +14,25 @@ function ModalCreateArticle({ onClose, refModal }: IProps): ReactElement {
       <GeneralModal onClose={() => onClose(false)} refModal={refModal}>
         <form className="flex w-full max-w-sm space-x-3">
           <div className="m-auto mt-10 w-full max-w-2xl rounded-lg bg-white px-5 py-10 shadow dark:bg-gray-800">
-            <div className="mb-6 text-center text-3xl font-light text-gray-800 dark:text-white">
+            <h1 className="mb-6 text-center text-3xl font-light text-gray-800 dark:text-white">
               {t("CREATE_ARTICLE")}
-            </div>
+            </h1>
             <div className="m-auto grid max-w-xl grid-cols-2 gap-4">
               <div className="col-span-2 lg:col-span-1">
                 <div className=" relative ">
                   <input
                     type="text"
-                    id="contact-form-name"
+                    id="article-form-title"
+                    className=" w-full flex-1 appearance-none rounded-lg border border-gray-300 border-transparent bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600"
+                    placeholder="Title"
+                  />
+                </div>
+              </div>
+              <div className="col-span-2 lg:col-span-1">
+                <div className=" relative ">
+                  <input
+                    type="text"
+                    id="article-form-type"
                     className=" w-full flex-1 appearance-none rounded-lg border border-gray-300 border-transparent bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600"
                     placeholder="Name"
                   />
@@ -32,9 +42,9 @@ function ModalCreateArticle({ onClose, refModal }: IProps): ReactElement {
                 <div className=" relative ">
                   <input
                     type="text"
-                    id="contact-form-email"
+                    id="article-author"
                     className=" w-full flex-1 appearance-none rounded-lg border border-gray-300 border-transparent bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600"
-                    placeholder="email"
+                    placeholder="Name"
                   />
                 </div>
               </div>
@@ -42,8 +52,8 @@ function ModalCreateArticle({ onClose, refModal }: IProps): ReactElement {
                 <label className="text-gray-700" htmlFor="name">
                   <textarea
                     className="w-full flex-1 appearance-none rounded-lg border border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600"
-                    id="comment"
-                    placeholder="Enter your comment"
+                    id="article-form-content"
+                    placeholder="Enter your Content"
                     name="comment"
                     rows={5}
                     cols={40}
