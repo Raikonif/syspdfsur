55a0624308fadc5211b0cef0c0dcd00a4ec5377d
Raikonif <raikonif@gmail.com>
2024-05-24 10:32:16 -0400

    implemented desing to cards in cases page

--- src/pages/blog_client/articles/Cases.tsx
+++ src/pages/blog_client/articles/Cases.tsx
@@ -19,7 +19,7 @@ function Cases(): ReactElement {
     const articles = await getArticles();
     setArticles(articles.data);
   };
-  const { cases } = useContext(ClientContext);
+  const { cases, handleClickOption } = useContext(ClientContext);
 
   useEffect(() => {
     allArticles().catch((error) => console.error(error));
@@ -52,16 +52,50 @@ function Cases(): ReactElement {
 
   return (
     <div className="flex w-full flex-col">
-      <div className="container mx-auto flex flex-col">
+      <div className="container mx-auto flex flex-col pt-20">
         <div className="flex w-full flex-col items-center justify-center md:flex-row">
           <Filter selected={changeFilter} setSelected={setChangeFilter} />
           <Search search={changeSearch} setSearch={setChangeSearch} />
         </div>
       </div>
       {/*<ArticleList articles={articlesFiltered} />*/}
-      {cases.map((_case) => (
-        <div key={_case.id}>{_case.title}</div>
-      ))}
+      <div className="flex flex-grow">
+        {cases.map((_case) => (
+          <div
+            className={`${
+              _case.type === "Histophatology" ? "border-indigo-700" : "border-cyan-700"
+            } m-6 flex w-full flex-grow flex-col rounded-2xl border-b-8 border-l border-r-8 border-t bg-slate-50 md:w-1/4`}
+            key={_case.id}
+          >
+            <div className="flex-1 overflow-hidden shadow">
+              <a href="#" className="flex flex-wrap no-underline hover:no-underline">
+                <h4
+                  className={`${
+                    _case.type === "Histophatology" ? "text-indigo-700" : "text-cyan-700"
+                  } w-full p-6 text-xs text-gray-600 md:text-sm`}
+                >
+                  {_case.type === "Histophatology" ? "Histopatológico" : "Citológíco"}
+                </h4>
+                <h2 className="w-full px-6 text-xl font-bold text-gray-800">Papanicolau</h2>
+                <p className="mb-5 px-6 text-base text-gray-800">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc
+                  commodo posuere et sit amet ligula.
+                </p>
+              </a>
+            </div>
+            <div className="overflow-hidde mt-auto flex-none p-6 shadow">
+              <div className="flex items-center justify-end">
+                <button
+                  className="gradient focus:shadow-outline mx-auto my-6 transform rounded-full bg-violet-600 px-8 py-4 font-bold text-white shadow-lg duration-300 ease-in-out transition hover:scale-105 hover:underline focus:outline-none dark:text-white lg:mx-0"
+                  onClick={() => handleClickOption("cases/1")}
+                >
+                  Leer mas ...
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
