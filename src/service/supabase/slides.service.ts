ca18aa7fc207ff0fac4dfe3deeec80ed96b483d2
Raikonif <raikonif@gmail.com>
2024-06-08 22:09:44 -0400

    cases and slide services code implemented

---
+++ src/service/supabase/slides.service.ts
@@ -0,0 +1,44 @@
+import supabase from "~/service/supabase/supabase.service";
+
+const getAllSlidesCases = async () => {
+  return supabase.from("slides").select();
+};
+
+const getSlideCase = async (slideId: string) => {
+  return supabase.from("slides").select().eq("id", slideId);
+};
+
+const getSlideFromCase = async (caseId: string) => {
+  return supabase.from("slides").select().eq("case_id", caseId);
+};
+
+const createSlideCase = async (data: object) => {
+  return supabase.from("slides").insert(data);
+};
+
+const updateSlideCase = async (slideId: string, data: object) => {
+  return supabase.from("slides").update(data).eq("id", slideId);
+};
+
+const updateSlideFromCase = async (caseId: string, data: object) => {
+  return supabase.from("slides").update(data).eq("case_id", caseId);
+};
+
+const deleteSlideCase = async (slideId: string) => {
+  return supabase.from("slides").delete().eq("id", slideId);
+};
+
+const deleteSlideFromCase = async (caseId: string) => {
+  return supabase.from("slides").delete().eq("case_id", caseId);
+};
+
+export {
+  getAllSlidesCases,
+  getSlideCase,
+  getSlideFromCase,
+  createSlideCase,
+  updateSlideCase,
+  updateSlideFromCase,
+  deleteSlideCase,
+  deleteSlideFromCase,
+};
