a30e7deb9f99e822b33e1fac68d600fc6ea76fe0
Raikonif <raikonif@gmail.com>
2024-06-27 16:55:22 -0400

    default paginator

--- src/pages/admin/cases/components/Paginator.tsx
+++ src/pages/admin/cases/components/Paginator.tsx
@@ -4,15 +4,7 @@ import { Pagination } from "@nextui-org/react";
 function Paginator() {
   return (
     <div className="hidden w-full justify-center lg:flex">
-      <Pagination
-        isCompact
-        showControls
-        total={10}
-        initialPage={1}
-        color="secondary"
-        size="lg"
-        variant="flat"
-      />
+      <Pagination isCompact showControls total={10} initialPage={1} color="secondary" size="lg" />
     </div>
   );
 }
