d673051b5e2ad4e5f4f74d6b845a931890a61f64
Raikonif <raikonif@gmail.com>
2024-07-04 11:29:59 -0400

    now can update data to backend service

--- src/service/digitalOceanSpaces.service.ts
+++ src/service/digitalOceanSpaces.service.ts
@@ -1,12 +1,9 @@
 import axios, { AxiosResponse } from "axios";
-import {
-  VITE_BACKEND_DO_SERVICE,
-  VITE_BACKEND_DO_SERVICE_MOCK,
-} from "~/constants/service/service.constants";
+import { VITE_BACKEND_DO_SERVICE } from "~/constants/service/service.constants";
 
 const uploadToDOSpaces = async (formData: FormData): Promise<AxiosResponse> => {
   try {
-    return await axios.post(`${VITE_BACKEND_DO_SERVICE_MOCK}/upload`, formData, {
+    return await axios.post(`${VITE_BACKEND_DO_SERVICE}/upload`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
