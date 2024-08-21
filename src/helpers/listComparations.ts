e4a72ec5a7b856415805fc56dd5d9f7d2e9d3539
Raikonif <raikonif@gmail.com>
2024-08-20 20:47:59 -0400

    now the update is working well

--- src/helpers/listComparations.ts
+++ src/helpers/listComparations.ts
@@ -26,50 +26,60 @@ function isTheSameLength(list1: OpSlidePreview[], list2: OpSlidePreview[]) {
 }
 
 async function listComparations({ originalList, comparationList, caseId }: Props) {
-  if (isTheSameLength(originalList, comparationList)) {
-    for (const slide of originalList) {
-      for (const comparationSlide of comparationList) {
-        if (comparationSlide.id === slide.id) {
-          if (!areTheSameAttributesAndValues(slide, comparationSlide)) {
-            if (
-              comparationSlide.image_url === slide.image_url &&
-              comparationSlide.image_url_webp === slide.image_url_webp &&
-              (comparationSlide.title !== slide.title ||
-                comparationSlide.description !== slide.description)
-            ) {
-              await updateSlideCase(comparationSlide.id, {
-                title: slide.title,
-                description: slide.description,
-              });
-            } else if (comparationSlide.image_url !== slide.image_url) {
-              const webpImg = await convertToWebp(slide.image_file);
-              const imgURL = await uploadDigitalOceanImg(slide.image_file);
-              const webpURL = await uploadDigitalOceanImg(webpImg);
-              const imgURLToDelete = comparationSlide.image_url.split("/").pop();
-              const webpURLToDelete = comparationSlide.image_url_webp.split("/").pop();
-              await deleteImageFromDOSpaces(imgURLToDelete);
-              await deleteImageFromDOSpaces(webpURLToDelete);
-              await updateSlideCase(comparationSlide.id, {
-                image_url: imgURL.data.file_url,
-                image_url_webp: webpURL.data.file_url,
-                title: slide.title,
-                description: slide.description,
-              });
-            }
+  if (originalList.length > 0 && comparationList.length === 0) {
+    await processAndUploadSlides(originalList, caseId);
+  }
+  for (const slide of originalList) {
+    if (slide.id === "" || slide.id === undefined) {
+      await createSlideCase({
+        case_id: caseId,
+        title: slide.title,
+        description: slide.description,
+        image_url: slide.image_url,
+        image_url_webp: slide.image_url_webp,
+      });
+      return;
+    }
+    for (const comparationSlide of comparationList) {
+      if (comparationSlide.id === slide.id) {
+        if (!areTheSameAttributesAndValues(slide, comparationSlide)) {
+          if (
+            comparationSlide.image_url === slide.image_url &&
+            comparationSlide.image_url_webp === slide.image_url_webp &&
+            (comparationSlide.title !== slide.title ||
+              comparationSlide.description !== slide.description)
+          ) {
+            await updateSlideCase(comparationSlide.id, {
+              title: slide.title,
+              description: slide.description,
+            });
+          } else if (comparationSlide.image_url !== slide.image_url) {
+            const webpImg = await convertToWebp(slide.image_file);
+            const imgURL = await uploadDigitalOceanImg(slide.image_file);
+            const webpURL = await uploadDigitalOceanImg(webpImg);
+            const imgURLToDelete = comparationSlide.image_url.split("/").pop();
+            const webpURLToDelete = comparationSlide.image_url_webp.split("/").pop();
+            await deleteImageFromDOSpaces(imgURLToDelete);
+            await deleteImageFromDOSpaces(webpURLToDelete);
+            await updateSlideCase(comparationSlide.id, {
+              image_url: imgURL.data.file_url,
+              image_url_webp: webpURL.data.file_url,
+              title: slide.title,
+              description: slide.description,
+            });
           }
-        } else if (slide.id === "" || slide.id === undefined) {
-          await createSlideCase({
-            case_id: caseId,
-            title: slide.title,
-            description: slide.description,
-            image_url: slide.image_url,
-            image_url_webp: slide.image_url_webp,
-          });
         }
       }
+      // else if (slide.id === "" || slide.id === undefined) {
+      //   await createSlideCase({
+      //     case_id: caseId,
+      //     title: slide.title,
+      //     description: slide.description,
+      //     image_url: slide.image_url,
+      //     image_url_webp: slide.image_url_webp,
+      //   });
+      // }
     }
-  } else if (originalList.length > 0 && comparationList.length === 0) {
-    await processAndUploadSlides(originalList, caseId);
   }
 }
 
