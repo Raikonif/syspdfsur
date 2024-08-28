0dca4f15a0de2a5ef3516baf33c0f3e9cb35b57d
Raikonif <raikonif@gmail.com>
2024-08-28 13:17:34 -0400

    implemented emailjs, need to configure email owner service to handle it

--- src/constants/service/service.constants.ts
+++ src/constants/service/service.constants.ts
@@ -1,2 +1,7 @@
 export const VITE_BACKEND_DO_SERVICE = import.meta.env.VITE_BACKEND_DO_SERVICE;
 export const VITE_BACKEND_DO_SERVICE_MOCK = import.meta.env.VITE_BACKEND_DO_SERVICE_MOCK;
+
+// EMAILJS
+export const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
+export const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
+export const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
